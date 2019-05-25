b8ca184e17dcbca9df29ddf050e4989d98ede1e9
Min Thu Wai <minthuwai.min@gmail.com>
2019-05-25 18:21:36 +0630

    first commit

---
+++ app/src/main/java/com/example/android/login/SelectRegionActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectRegionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_region);
+    }
+}
